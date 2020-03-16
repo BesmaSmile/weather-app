049a4bd9601e9b84093364f0f58cae1860d723f4
Besma <db_rabia_cherif@edu.esi.dz>
2020-03-16 07:33:42 +0100

    reducers constants added

---
+++ src/constants/reducers.constants.js
@@ -0,0 +1,9 @@
+export const reducersConstants={
+    weatherConstants,
+}
+
+const weatherConstants = {
+    WEATHER_REQUEST : 'WEATHER_REQUEST',
+    WEATHER_SUCCESS : 'WEATHER_SUCCESS',
+    WEATHER_FAILURE : 'WEATHER_FAILURE',
+}
