1395dfbda348261368352057ea3b10489a0f8600
Besma <db_rabia_cherif@edu.esi.dz>
2020-03-16 07:52:02 +0100

    OpenWeatherMap endPoint updated

--- src/constants/api.constants.js
+++ src/constants/api.constants.js
@@ -1,4 +1,4 @@
 const apiConstants = {
-    OPEN_WEATHER_MAP_END_POINT : 'api.openweathermap.org',
+    OPEN_WEATHER_MAP_END_POINT : 'api.openweathermap.org/data/2.5/weather',
     OPEN_WEATHER_MAP_KEY : 'a485c20d5b7867dc32d2cff59f70f2f4'
 }
